9e26b397017689f2efcc5e2f5a0a4c2d0aaad7a4
Tim Mastny <tim.mastny@gmail.com>
2025-01-26 10:20:27 -0600

    fix local and prod

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
     },
   },
+  // Use base: '/rga/' only for production builds
+  base: command === 'build' ? '/rga/' : '/',
   build: {
     sourcemap: true
   }
-}) 
+})) 
